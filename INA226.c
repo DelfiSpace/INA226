1865583107b1096bc8285a364a95500377908396
nchronas <nchronas@gmail.com>
2018-05-08 19:58:02 +0200

    Adding capabilities

--- INA226.c
+++ INA226.c
@@ -14,6 +14,7 @@
 
 #include "INA226.h"
 #include "hal_uart.h"
+#include <math.h>
 
 #define CONFIGURATION    0x00
 #define SHUNT            0x01
@@ -27,6 +28,41 @@
 #define RESET            0x8000
 #define CALIBRATION_REF  40.96f
 
+
+
+#define CONFIGURATION    0x00
+#define SHUNT            0x01
+#define VOLTAGE          0x02
+#define POWER            0x03
+#define CURRENT          0x04
+#define CALIBRATION      0x05
+#define ID               0xFF
+
+#define DEVICE_ID        0x2260
+#define RESET            0x8000
+#define CALIBRATION_REF  40.96f
+
+
+#define INA226_REG_CONFIG           (0x00)
+#define INA226_REG_SHUNTVOLTAGE     (0x01)
+#define INA226_REG_BUSVOLTAGE       (0x02)
+#define INA226_REG_POWER            (0x03)
+#define INA226_REG_CURRENT          (0x04)
+#define INA226_REG_CALIBRATION      (0x05)
+#define INA226_REG_MASKENABLE       (0x06)
+#define INA226_REG_ALERTLIMIT       (0x07)
+
+#define INA226_BIT_SOL              (0x8000)
+#define INA226_BIT_SUL              (0x4000)
+#define INA226_BIT_BOL              (0x2000)
+#define INA226_BIT_BUL              (0x1000)
+#define INA226_BIT_POL              (0x0800)
+#define INA226_BIT_CNVR             (0x0400)
+#define INA226_BIT_AFF              (0x0010)
+#define INA226_BIT_CVRF             (0x0008)
+#define INA226_BIT_OVF              (0x0004)
+#define INA226_BIT_APOL             (0x0002)
+#define INA226_BIT_LEN              (0x0001)
 
 /**
  *
@@ -42,7 +78,15 @@
  */
 uint8_t ina_readRegister(const dev_id id, uint8_t reg, uint16_t *res)
 {
-    HAL_I2C_readWrite(id, &reg, 1, res, 2);
+
+    uint8_t rx_buf[3] = { reg, 0, 0 };
+
+    HAL_I2C_readWrite(id, rx_buf, 1, res, 2);
+
+    uint16_t temp;
+    temp = *res;
+    *res = ((0x00ff & temp) << 8);
+    *res |= (temp >> 8);
 
     return 0;
 }
@@ -60,9 +104,166 @@ uint8_t ina_readRegister(const dev_id id, uint8_t reg, uint16_t *res)
 void ina_writeRegister(const dev_id id, const uint8_t reg, const uint16_t val)
 {
     uint8_t tx_buf[3] = { reg, ((val >> 8) & 0xFF), (val & 0xFF) };
+ //   uint8_t rx_buf[3] = { };
 
+//    HAL_I2C_readWrite(id, tx_buf, 3, rx_buf, 0);
     HAL_I2C_readWrite(id, tx_buf, 3, NULL, 0);
 
+}
+
+uint16_t INA226_getConfig(const dev_id id)
+{
+    uint16_t value;
+    ina_readRegister(id, INA226_REG_CONFIG, &value);
+
+    return value;
+}
+
+uint16_t INA226_getCalibration(const dev_id id)
+{
+    uint16_t value;
+    ina_readRegister(id, INA226_REG_CALIBRATION, &value);
+
+    return value;
+}
+
+void INA226_configure(const dev_id id,
+                      ina226_averages_t avg,
+                      ina226_busConvTime_t busConvTime,
+                      ina226_shuntConvTime_t shuntConvTime,
+                      ina226_mode_t mode)
+{
+    uint16_t config = 0;
+
+    config |= (avg << 9 | busConvTime << 6 | shuntConvTime << 3 | mode);
+
+    ina_writeRegister(id, INA226_REG_CONFIG, config);
+}
+
+bool INA226_calibrate(const dev_id id,
+                      float rShunt,
+                      float iMaxExpected,
+                      float *currentLSB,
+                      float *powerLSB)
+{
+    uint16_t calibrationValue;
+
+    //float vBusMax = 36;
+    //float vShuntMax = 0.08192f;
+
+    //float iMaxPossible;
+    //iMaxPossible = vShuntMax / rShunt;
+    float minimumLSB;
+
+    minimumLSB = iMaxExpected / 32767;
+
+    *currentLSB = (uint16_t)(minimumLSB * 100000000);
+    *currentLSB /= 100000000;
+    *currentLSB /= 0.0001;
+    *currentLSB = ceil(*currentLSB);
+    *currentLSB *= 0.0001;
+
+    *powerLSB = *currentLSB * 25;
+
+    calibrationValue = (uint16_t)((0.00512) / (*currentLSB * rShunt));
+
+    ina_writeRegister(id, INA226_REG_CALIBRATION, calibrationValue);
+
+    ina_readRegister(id, INA226_REG_CALIBRATION, &calibrationValue);
+
+    return true;
+}
+
+float INA226_readBusPower(const dev_id id, float powerLSB)
+{
+    int16_t power;
+
+    ina_readRegister(id, INA226_REG_POWER, &power);
+
+    return (power * powerLSB);
+}
+
+float INA226_readShuntCurrent(const dev_id id, float currentLSB)
+{
+    int16_t current;
+
+    ina_readRegister(id, INA226_REG_CURRENT, &current);
+
+    return (current * currentLSB);
+}
+
+float INA226_readShuntVoltage(const dev_id id)
+{
+    int16_t voltage;
+
+    ina_readRegister(id, INA226_REG_SHUNTVOLTAGE, &voltage);
+
+    return (voltage * 0.0000025);
+}
+
+float INA226_readBusVoltage(const dev_id id)
+{
+    uint16_t voltage;
+
+    ina_readRegister(id, INA226_REG_BUSVOLTAGE, &voltage);
+
+    return (voltage * 0.00125);
+}
+
+float INA226_rawBusPower(int16_t power, float powerLSB) {
+
+     return (power * powerLSB);
+}
+
+float INA226_rawShuntCurrent(int16_t current, float currentLSB)
+{
+     return (current * currentLSB);
+}
+
+float INA226_rawShuntVoltage(int16_t voltage)
+{
+     return (voltage * 0.0000025);
+}
+
+float INA226_rawBusVoltage(uint16_t voltage)
+{
+     return (voltage * 0.00125);
+}
+
+uint16_t INA226_readBusPower_raw(const dev_id id)
+{
+    uint16_t power;
+
+    ina_readRegister(id, INA226_REG_POWER, &power);
+
+    return (power);
+}
+
+uint16_t INA226_readShuntCurrent_raw(const dev_id id)
+{
+    uint16_t current;
+
+    ina_readRegister(id, INA226_REG_CURRENT, &current);
+
+    return (current);
+}
+
+uint16_t INA226_readShuntVoltage_raw(const dev_id id)
+{
+    uint16_t voltage;
+
+    ina_readRegister(id, INA226_REG_SHUNTVOLTAGE, &voltage);
+
+    return (voltage);
+}
+
+uint16_t INA226_readBusVoltage_raw(const dev_id id)
+{
+    uint16_t voltage;
+
+    ina_readRegister(id, INA226_REG_BUSVOLTAGE, &voltage);
+
+    return (voltage);
 }
 
 /**
@@ -70,7 +271,7 @@ void ina_writeRegister(const dev_id id, const uint8_t reg, const uint16_t val)
  *   Reset the INA226
  *
  */
-void ina_reset(dev_id id)
+void INA226_reset(dev_id id)
 {
    ina_writeRegister(id, CONFIGURATION, RESET);
 }
@@ -84,83 +285,12 @@ void ina_reset(dev_id id)
  *   false                 otherwise
  *
  */
-bool ina_readDeviceID(dev_id id)
+bool INA226_readDeviceID(dev_id id)
 {
     uint16_t res = 0;
-    ina_readRegister(id, ID, &res);
+    uint8_t reg = ID;
+
+    ina_readRegister(id, &reg, &res);
 
     return res == DEVICE_ID;
-}
-
-/**
- *
- *   Sets the shunt resistor value in Ohm
- *
- *   Parameters:
- *   double shunt          shunt resistor value in Ohm
- *
- */
-void ina_setShuntResistor(dev_id id, double shunt)
-{
-    ina_writeRegister(id, CALIBRATION, (uint16_t)(CALIBRATION_REF / shunt));
-}
-
-/**
- *
- *   Returns the bus voltage in mV
- *
- *   Returns:
- *   unsigned short        bus voltage in mV
- *
- */
-uint16_t ina_getVoltage(dev_id id)
-{
-    uint16_t res = 0;
-    ina_readRegister(id, VOLTAGE, &res);
-    return res + (res >> 2);
-}
-
-/**
- *
- *   Returns the voltage across the shunt resistor
- *
- *   Returns:
- *   signed short        bus voltage (LSB = 2.5 uV)
- *
- */
-int16_t ina_getShuntVoltage(dev_id id)
-{
-    uint16_t res = 0;
-    ina_readRegister(id, SHUNT, &res);
-    return res;
-}
-
-/**
- *
- *   Returns the current through the shunt resistor
- *
- *   Returns:
- *   signed short         current in mA
- *
- */
-int16_t ina_getCurrent(dev_id id)
-{
-    uint16_t res = 0;
-    ina_readRegister(id, CURRENT, &res);
-    return (res >> 3);
-}
-
-/**
- *
- *   Returns the power across the load in mW
- *
- *   Returns:
- *   unsigned short        power in mW
- *
- */
-uint16_t ina_getPower(dev_id id)
-{
-    uint16_t res = 0;
-    ina_readRegister(id, POWER, &res);
-    return (res * 3) + (res >> 3);
 }
--- INA226.h
+++ INA226.h
@@ -19,18 +19,89 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+
+typedef enum
+{
+    INA226_AVERAGES_1             = 0b000,
+    INA226_AVERAGES_4             = 0b001,
+    INA226_AVERAGES_16            = 0b010,
+    INA226_AVERAGES_64            = 0b011,
+    INA226_AVERAGES_128           = 0b100,
+    INA226_AVERAGES_256           = 0b101,
+    INA226_AVERAGES_512           = 0b110,
+    INA226_AVERAGES_1024          = 0b111
+} ina226_averages_t;
+
+typedef enum
+{
+    INA226_BUS_CONV_TIME_140US    = 0b000,
+    INA226_BUS_CONV_TIME_204US    = 0b001,
+    INA226_BUS_CONV_TIME_332US    = 0b010,
+    INA226_BUS_CONV_TIME_588US    = 0b011,
+    INA226_BUS_CONV_TIME_1100US   = 0b100,
+    INA226_BUS_CONV_TIME_2116US   = 0b101,
+    INA226_BUS_CONV_TIME_4156US   = 0b110,
+    INA226_BUS_CONV_TIME_8244US   = 0b111
+} ina226_busConvTime_t;
+
+typedef enum
+{
+    INA226_SHUNT_CONV_TIME_140US   = 0b000,
+    INA226_SHUNT_CONV_TIME_204US   = 0b001,
+    INA226_SHUNT_CONV_TIME_332US   = 0b010,
+    INA226_SHUNT_CONV_TIME_588US   = 0b011,
+    INA226_SHUNT_CONV_TIME_1100US  = 0b100,
+    INA226_SHUNT_CONV_TIME_2116US  = 0b101,
+    INA226_SHUNT_CONV_TIME_4156US  = 0b110,
+    INA226_SHUNT_CONV_TIME_8244US  = 0b111
+} ina226_shuntConvTime_t;
+
+typedef enum
+{
+    INA226_MODE_POWER_DOWN      = 0b000,
+    INA226_MODE_SHUNT_TRIG      = 0b001,
+    INA226_MODE_BUS_TRIG        = 0b010,
+    INA226_MODE_SHUNT_BUS_TRIG  = 0b011,
+    INA226_MODE_ADC_OFF         = 0b100,
+    INA226_MODE_SHUNT_CONT      = 0b101,
+    INA226_MODE_BUS_CONT        = 0b110,
+    INA226_MODE_SHUNT_BUS_CONT  = 0b111,
+} ina226_mode_t;
+
+
+void INA226_configure(const dev_id id,
+                      ina226_averages_t avg,
+                      ina226_busConvTime_t busConvTime,
+                      ina226_shuntConvTime_t shuntConvTime,
+                      ina226_mode_t mode);
+
+bool INA226_calibrate(const dev_id id,
+                      float rShunt,
+                      float iMaxExpected,
+                      float *currentLSB,
+                      float *powerLSB);
+
+uint16_t INA226_getConfig(const dev_id id);
+
+uint16_t INA226_getCalibration(const dev_id id);
+
+float INA226_readBusPower(const dev_id id, float powerLSB);
+float INA226_readShuntCurrent(const dev_id id, float currentLSB);
+float INA226_readShuntVoltage(const dev_id id);
+float INA226_readBusVoltage(const dev_id id);
+
+uint16_t INA226_readBusPower_raw(const dev_id id);
+uint16_t INA226_readShuntCurrent_raw(const dev_id id);
+uint16_t INA226_readShuntVoltage_raw(const dev_id id);
+uint16_t INA226_readBusVoltage_raw(const dev_id id);
+
+float INA226_rawBusPower(int16_t power, float powerLSB);
+float INA226_rawShuntCurrent(int16_t current, float currentLSB);
+float INA226_rawShuntVoltage(int16_t voltage);
+float INA226_rawBusVoltage(uint16_t voltage);
+
 void ina_reset(dev_id id);
 
 bool ina_readDeviceID(dev_id id);
-
-void ina_setShuntResistor(dev_id id, double shunt);
-
-uint16_t ina_getVoltage(dev_id id);
-
-int16_t ina_getShuntVoltage(dev_id id);
-
-int16_t ina_getCurrent(dev_id id);
-
-uint16_t ina_getPower(dev_id id);
 
 #endif // __INA226_H__
